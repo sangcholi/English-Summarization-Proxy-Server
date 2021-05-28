5d35b3ac8f6d4a7ea578af5cc67cf0110be8d08c
naemoo <gusdn0657@gmail.com>
2021-05-28 20:56:16 +0900

    feat: init

---
+++ src/test/java/com/ssu/sangcholi/englishsummarizationservice/EnglishSummarizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssu.sangcholi.englishsummarizationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishSummarizationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
